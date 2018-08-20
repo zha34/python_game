d16daee1449725819215033e0cefe28025039640
zha34 <zha34@sfu.ca>
2018-08-19 18:00:57 -0700

    add rounds and ramdon number

--- typedis/typedis.py
+++ typedis/typedis.py
@@ -2,12 +2,31 @@
 
 '''
 typedis
-	compute & display score
-	typing a-z
-	set num of times in a round
+  compute & display score
+  typing a-z
+  set num of times in a round
 '''
+import random
 
 
-print("Welcome to Typedis!")
+letter = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+ 			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'x', 't', 
+ 			'u', 'v', 'w', 's', 'y', 'z' ]
 
 
+
+print("\nWelcome to Typedis!\n")
+
+rounds = "invalid"
+
+while not rounds.isdigit(): 
+  rounds = input("Enter the number of rounds: ")
+  print("Random int: ", random.randint(0,25))
+
+rounds = int(rounds)
+print("The number of rounds is: ", rounds)
+print("The number of rounds is: ", rounds)
+
+
+
+print("\nCya!\n")
