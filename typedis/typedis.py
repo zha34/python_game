fa0075c90e568ef7119985a148fd182a7870da89
zha34 <zha34@sfu.ca>
2018-08-21 13:21:19 -0700

    add scoring for user input

--- typedis/typedis.py
+++ typedis/typedis.py
@@ -9,24 +9,47 @@ typedis
 import random
 
 
+
+def randWord( rounds ):
+  for i in range(rounds):
+  	question = letter[random.randint(0,25)]
+  	yield question
+  	
+
+
 letter = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
  			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'x', 't', 
  			'u', 'v', 'w', 's', 'y', 'z' ]
 
-
+rounds = "invalid"
+score = 0
+size = 3
 
 print("\nWelcome to Typedis!\n")
 
-rounds = "invalid"
 
-while not rounds.isdigit(): 
-  rounds = input("Enter the number of rounds: ")
-  print("Random int: ", random.randint(0,25))
 
+# while not rounds.isdigit():  # loop until a num is entered
+#   rounds = input("Enter the number of rounds: ")
+
+rounds = 5
 rounds = int(rounds)
 print("The number of rounds is: ", rounds)
-print("The number of rounds is: ", rounds)
 
 
+# 1 question per rounds
+for i in range(rounds):
+  print("\nRound ", i+1, ":")
+  
 
+  question = letter[random.randint(0,25)]
+
+
+  print(question)
+  answer = input()
+  if answer == question:
+  	score += 1
+
+
+print("\nYour score is: ", score)
 print("\nCya!\n")
